bf7b42b094f68565f57e0790e3acdb32b6ce9192
Stefan Terdell <stefan.terdell+github@proton.me>
2024-04-16 18:14:41 +0200

    Rename all mod.rs files

---
+++ src/blog.rs
@@ -0,0 +1,8 @@
+mod components;
+mod models;
+mod server_fns;
+mod utils;
+
+pub use components::BlogPost::*;
+pub use components::BlogPosts::*;
+pub use components::EditBlogPost::*;
--- src/blog/mod.rs
+++
@@ -1,8 +0,0 @@
-mod components;
-mod models;
-mod server_fns;
-mod utils;
-
-pub use components::BlogPost::BlogPost;
-pub use components::BlogPosts::BlogPosts;
-pub use components::EditBlogPost::EditBlogPost;
